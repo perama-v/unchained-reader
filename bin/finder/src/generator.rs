26a75b0dfca2b50cbcc1b48dd75b558527b030c4
perama-v <83961755+perama-v@users.noreply.github.com>
2023-07-27 18:31:14 +1000

    feat(bin): generate test vector

---
+++ bin/finder/src/generator.rs
@@ -0,0 +1,41 @@
+//! Generates test case JSON-RPC responses
+
+use serde::{Deserialize, Serialize};
+use unchained_utils::structure::AddressData;
+
+
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AddressesInBlockResponse {
+    pub id: u32,
+    pub jsonrpc: String,
+    pub result: Vec<AddressAppearance>,
+}
+
+
+impl AddressesInBlockResponse {
+    pub fn create(data: Vec<AddressData>) -> Self {
+        AddressesInBlockResponse {
+            id: 1,
+            jsonrpc: "2.0".to_string(),
+            result: data.into_iter().map(|x| {
+                let address = hex::encode(x.address);
+                let appearances = x.appearances.into_iter().map(|y| {
+                    y.index
+                }).collect();
+
+                AddressAppearance { address, appearances }
+            }).collect(),
+        }
+    }
+}
+
+/// Holds selected transactions for a given address.
+#[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
+pub struct AddressAppearance {
+    /// The address that appeared in a transaction.
+    pub address: String,
+    /// The transaction index where the address appeared.
+    pub appearances: Vec<u32>,
+}
--- bin/finder/src/main.rs
+++ bin/finder/src/main.rs
@@ -1,17 +1,18 @@
-
 pub mod cli;
+mod generator;
 
 use clap::Parser;
 use cli::AppArgs;
-use unchained_utils::{UnchainedFile, BlockRange};
+use generator::AddressesInBlockResponse;
+use unchained_utils::{BlockRange, UnchainedFile};
+
 
 fn main() {
     let args = AppArgs::parse();
     let range = BlockRange::new(args.low, args.high).expect("Bad range");
     let mut file = UnchainedFile::from_file(args.name, range).expect("Couldn't read file");
-    file.with_parsed("9a");
-    for tx in file.parsed {
-        println!("{:?} in \t\t{:?}", tx.address, tx.appearances);
-    }
-}
+    file.with_parsed(None).expect("Could not add appearance data");
+    let response = AddressesInBlockResponse::create(file.parsed);
+    println!("{}",serde_json::to_string(&response).expect("Could not create JSON response"));
 
+}
