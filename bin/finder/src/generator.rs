d238723238ed3e76411f43d89871c033307333bd
perama-v <83961755+perama-v@users.noreply.github.com>
2023-07-27 22:35:04 +1000

    feat(finder): change appearances to transactionIndices in response

--- bin/finder/src/generator.rs
+++ bin/finder/src/generator.rs
@@ -3,8 +3,6 @@
 use serde::{Deserialize, Serialize};
 use unchained_utils::structure::AddressData;
 
-
-
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct AddressesInBlockResponse {
@@ -21,11 +19,11 @@ impl AddressesInBlockResponse {
             jsonrpc: "2.0".to_string(),
             result: data.into_iter().map(|x| {
                 let address = hex::encode(x.address);
-                let appearances = x.appearances.into_iter().map(|y| {
+                let transaction_indices = x.appearances.into_iter().map(|y| {
                     y.index
                 }).collect();
 
-                AddressAppearance { address, appearances }
+                AddressAppearance { address, transaction_indices }
             }).collect(),
         }
     }
@@ -33,9 +31,10 @@ impl AddressesInBlockResponse {
 
 /// Holds selected transactions for a given address.
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct AddressAppearance {
     /// The address that appeared in a transaction.
     pub address: String,
     /// The transaction index where the address appeared.
-    pub appearances: Vec<u32>,
+    pub transaction_indices: Vec<u32>,
 }
